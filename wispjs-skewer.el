1bde86a70c7320d2cf5d0b33950ac6613b4d5772
natto <natto@natto-mba.local>
2015-04-02 14:44:43 -0400

    ADD wispjs skewer interaction

---
+++ wispjs-skewer.el
@@ -0,0 +1,31 @@
+(defun wispjs-skewer-send-region (beg end)
+ (let ((sbuf (current-buffer)))
+   (with-temp-buffer
+     (let ((tbuf (current-buffer)))
+       (set-buffer sbuf)
+       (call-process-region beg end
+        wisp-program nil tbuf nil "--no-map")
+       (set-buffer tbuf)
+       (skewer-eval (buffer-string))))))
+
+(defun wispjs-skewer-eval-preceding-sexp ()
+  (interactive)
+  (save-excursion
+    (let ((beg (progn (beginning-of-sexp)
+                      (point)))
+          (end (progn (end-of-sexp)
+                      (point))))
+      (wispjs-skewer-send-region beg end))))
+
+(defun wispjs-skewer-eval-defun ()
+  (interactive)
+  (save-excursion
+    (let ((beg (progn (beginning-of-defun)
+                      (point)))
+          (end (progn (end-of-defun)
+                      (point))))
+      (wispjs-skewer-send-region beg end))))
+
+(define-key wispjs-mode-map (kbd "C-x C-e") 'wispjs-skewer-eval-preceding-sexp)
+(define-key wispjs-mode-map (kbd "C-M-x") 'wispjs-skewer-eval-defun)
+
